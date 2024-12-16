57ea67ceb92191acf6573c0d5d802702fdf9d1e8
Naseem Ansari <mrnaseem745@gmail.com>
2024-12-16 19:19:24 +0530

    unused vars errors fix

--- components/Buttons/FacebookButton/FacebookButton.tsx
+++ components/Buttons/FacebookButton/FacebookButton.tsx
@@ -7,12 +7,14 @@ interface FacebookButtonProps {
 const FacebookButton: React.FC<FacebookButtonProps> = ({ label, onClick }) => {
   return (
     <div className="flex flex-col justify-center items-center gap-1">
-      <button className="min-w-fit flex justify-center items-center bg-[#1877f2] text-white hover:bg-[#1877f2]/90 py-2 px-6 rounded-md">
+      <div
+      onClick={onClick}
+      className="min-w-fit flex justify-center items-center bg-[#1877f2] text-white hover:bg-[#1877f2]/90 py-2 px-6 rounded-md">
       <span className="pointer-events-none mr-2">
         <FaFacebook className="opacity-60"/>
       </span>
-      Login
-    </button>
+      {label}
+    </div>
     </div>
   );
 }
--- components/Buttons/XButton/XButton.tsx
+++ components/Buttons/XButton/XButton.tsx
@@ -7,12 +7,14 @@ interface XButtonProps {
 const XButton: React.FC<XButtonProps> = ({ label, onClick }) => {
   return (
     <div className="flex flex-col justify-center items-center gap-1">
-      <button className="min-w-fit flex justify-center items-center bg-[#14171a] text-white hover:bg-[#14171a]/90 py-2 px-6 rounded-md">
+      <div
+      onClick={onClick}
+      className="min-w-fit flex justify-center items-center bg-[#14171a] text-white hover:bg-[#14171a]/90 py-2 px-6 rounded-md">
       <span className="pointer-events-none mr-2">
         <FaXTwitter className="opacity-60"/>
       </span>
-      Login
-    </button>
+      {label}
+    </div>
     </div>
   );
 }
