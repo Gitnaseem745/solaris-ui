d7ec1980588a798343858f5f45ddf605f4349a64
Naseem Ansari <mrnaseem745@gmail.com>
2025-01-09 22:48:02 +0530

    error fixes

--- components/site/Navbar/Navbar.tsx
+++ components/site/Navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image"
+// import Image from "next/image"
 import Link from "next/link"
 import { FaGithub } from "react-icons/fa"
 import ThemeToggle from "../ThemeToggle/ThemeToggle"
