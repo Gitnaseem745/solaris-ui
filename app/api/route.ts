e1fea27e19d7976f5adaaa4acbcd354f41718e4a
Naseem Ansari <mrnaseem745@gmail.com>
2025-01-11 12:31:52 +0530

    relocated for buttons dir errors

--- app/api/route.ts
+++ app/api/route.ts
@@ -17,7 +17,6 @@ export async function GET(request: Request) {
   const filePath = path.join(
     process.cwd(),
     "components",
-    "ui",
     resolvedDirName,
     `${componentName}.tsx`
   );
