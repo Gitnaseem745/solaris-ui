7c963f85305bdcb1f80c93e9fa587b43dc98748c
Naseem Ansari <mrnaseem745@gmail.com>
2025-01-13 14:41:34 +0530

    Fix folder naming to consistent casing

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,7 @@
-/** @type {import('next').NextConfig} */
-const fs = require('fs');
-const path = require('path');
-
 const nextConfig = {
     typescript: {
         ignoreBuildErrors: true,
-      },
-      webpack: (config) => {
-        config.resolve.alias = {
-          ...config.resolve.alias,
-          '@': path.resolve(__dirname, './'),
-        };
-
-        return config;
-      },
+      }
 };
 
 module.exports = nextConfig;
