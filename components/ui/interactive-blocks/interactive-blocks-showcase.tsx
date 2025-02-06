6a8a6b12776e97a5e476fe55ea7034339bc8d42a
Naseem Ansari <mrnaseem745@gmail.com>
2025-02-06 16:45:57 +0530

    add SocialTagShowcase component for displaying social links

---
+++ components/ui/interactive-blocks/interactive-blocks-showcase.tsx
@@ -0,0 +1,9 @@
+import SocialTag from "./social-tag"
+
+export const SocialTagShowcase = () => {
+    return (
+        <div className="max-w-lg w-full">
+        <SocialTag title="Twitter" id="@solaris_ui" />
+        </div>
+    )
+}
