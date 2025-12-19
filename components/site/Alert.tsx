e47a0989d4b4e675e4f718be0c7be0f6a215f9c6
Naseem Ansari <118877331+Gitnaseem745@users.noreply.github.com>
2025-12-20 01:49:29 +0530

    Update NextLaunch link in Alert component

--- components/site/Alert.tsx
+++ components/site/Alert.tsx
@@ -6,7 +6,7 @@ const DocsAlert: React.FC = () => {
           className="bg-indigo-600 text-white p-4 z-50"
         >
           <div className="container mx-auto flex justify-start items-center font-sans">
-            <Link href={'https://nextlaunch.solarisui.com/'} target="_blank" className="text-sm text-center font-semibold w-full max-sm:text-xs"> ✨ Introducing NextLaunch — Premium templates designed to help you build stunning landing pages in minutes.
+            <Link href={'https://nextlaunch.calsinas.app/'} target="_blank" className="text-sm text-center font-semibold w-full max-sm:text-xs"> ✨ Introducing NextLaunch — Premium templates designed to help you build stunning landing pages in minutes.
             </Link>
           </div>
         </div>
